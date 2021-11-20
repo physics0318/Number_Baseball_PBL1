add248fbbb010b38edb871b145ffdd7dd344890e
Your Name <you@example.com>
2021-11-21 00:30:53 +0900

    수학이 완성

--- Baseballgame.py
+++ Baseballgame.py
@@ -1,14 +1,14 @@
 import Baseball동국
-import Baseball수학4_1
+import Baseball수학
 
 givenumber = input("숫자를 입력하시오: ")
 Dgu = Baseball동국.Dongguk(givenumber)
-b = Baseball수학4_1.guess()
+b = Baseball수학.guess()
 
-print(b.total.index(list(map(int, givenumber))))
+# print(b.total.index(list(map(int, givenumber))))
 
 while Dgu.strike < 4:
-    num = b.guess()
+    num = b.randGuess()
     print(num)
     Dgu.evaluate(num)
     b.eliminate(num, Dgu.strike, Dgu.ball)
--- Baseball수학.py
+++ Baseball수학.py
@@ -8,6 +8,8 @@ class guess():
 
     def eliminate(self, l, strike, ball):                               #strike, ball의 입력에 따라 각 자릿수에 올만한 숫자를 정리하는 함수
         self.history.append([l,strike,ball])                            #과거에 했던 대답과 그에 따른 strike, ball을 기억
+        if strike == 4:
+            return
         if strike == 0:                                                 #strike가 0일 경우
             for i in range(4):
                 if l[i] in self.numbers[i]:                             #그 자릿수에 올만한 숫자들에서 방금 찍었던 답과 같은자리의 숫자를 제거
@@ -21,7 +23,9 @@ class guess():
 
         if strike + ball == 4:                                          #s+b = 4 이면
             for i in range(4):
-                self.numbers[i] = l                                     #방금 찍었던 답안의 숫자들만 사용
+                for j in self.numbers[i]:                                     #방금 찍었던 답안의 숫자들만 사용
+                    if j not in l:
+                        self.numbers[i].remove(j)
 
     def randGuess(self):
         self.iter += 1
@@ -30,24 +34,26 @@ class guess():
         elif self.iter == 2:
             return [5,6,7,8]
         else:
-            X = self.numbers
             l = []
             while len(l)<4:
-                for i in X:
+                for i in self.numbers:
                     n = random.choice(i)
                     if n in l:
                         l=[]
                         break
                     else:
                         l.append(n)
+
+                if len(l) == 4:
                     for j in self.history:                              #과거 기록을 참고하여 랜덤으로 뽑을 숫자를 한정 (예: [1,2,3,4]가 0s 2b였다면 1~4중에서는 2개만 뽑도록 함)
-                            cnt = 0
-                            for k in l:
-                                if k in j[0]:
-                                    cnt += 1
-                            if cnt > j[1]+j[2]:
-                                l=[]
-                                break
+                        cnt = 0
+                        for k in l:
+                            if k in j[0]:
+                                cnt += 1
+                        if cnt != j[1]+j[2]:
+                            l=[]
+                            break
+                
                 if len(l) > 4:
                     l = []
                     continue
